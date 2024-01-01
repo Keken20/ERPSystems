208a2f48a82d589324b2e060765da37b7a70bfa3
Aiken Canama <Aiken Canama@LAPTOP-RAPGCP1J>
2024-01-02 02:28:19 +0800

    UPDATED TEMPLATE

---
+++ ERPSystems/Controllers/CustodianPageController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERPSystems.Controllers
+{
+    public class CustodianPageController : Controller
+    {
+        // GET: CustodianPage
+        public ActionResult CustodianDashboard()
+        {
+            return View();
+        }
+        public ActionResult CustodianPurchaseOrder()
+        {
+            return View();
+        }
+        public ActionResult CustodianInventory()
+        {
+            return View();
+        }
+    }
+}
